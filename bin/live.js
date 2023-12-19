ee2f239645ab49b4f12eb2ee5e308c4ceab6b2e7
othelarian <le.maitre.killian@gmail.com>
2023-12-19 21:49:15 +0100

    Fixing argv issue

--- bin/live.js
+++ bin/live.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-var fs, ls, optionator, ref$, List, Obj, Str, options, option, tasks, task, prepend, mlgth, filler, line, append, op, args, opts, e;
+var fs, ls, optionator, ref$, List, Obj, options, option, tasks, task, prepend, mlgth, filler, line, append, op, args, opts, e;
 fs = require('fs');
 ls = require('livescript');
 optionator = require('optionator');
-ref$ = require('prelude-ls'), List = ref$.List, Obj = ref$.Obj, Str = ref$.Str;
+ref$ = require('prelude-ls'), List = ref$.List, Obj = ref$.Obj;
 options = [{
   option: 'help',
   alias: 'h',
@@ -29,7 +29,8 @@ task = function(name, desc, fn){
 global.option = option;
 global.task = task;
 try {
-  ls.run(fs.readFileSync('Livefile', 'utf-8'));
+  ls.run(
+  fs.readFileSync('Livefile', 'utf-8'));
   prepend = 'Usage: live [options]\n\nOptions:';
   mlgth = function(it){
     return it.length;
@@ -39,10 +40,10 @@ try {
   })(
   Obj.keys(tasks)));
   filler = function(str){
-    return Str.repeat(mlgth - str.length, ' ');
+    return repeatString$(' ', mlgth - str.length);
   };
   line = function(acc, elt){
-    return acc + "" + elt[0] + filler(elt[0]) + " " + elt[1].desc + "\n";
+    return acc + "" + elt[0] + filler(elt[0]) + "  " + elt[1].desc + "\n";
   };
   append = List.fold(line, 'Commands:\n\n')(
   Obj.objToPairs(tasks));
@@ -51,10 +52,18 @@ try {
     options: options,
     prepend: prepend
   });
-  args = new RegExp('node.exe').test(process.argv[0])
-    ? process.argv.slice(1)
+  args = new RegExp('lsc').test(process.argv[1])
+    ? process.argv.splice(1, 1)
     : process.argv;
   opts = op.parseArgv(args);
+  opts = function(it){
+    return it.parseArgv(process.argv);
+  }(
+  optionator({
+    append: append,
+    options: options,
+    prepend: prepend
+  }));
   switch (false) {
   case !opts.help:
     console.log(op.generateHelp());
@@ -66,7 +75,7 @@ try {
     console.log('ERROR: too many tasks selected!');
     break;
   case in$(opts._[0], Obj.keys(tasks)):
-    console.log('ERROR: task does not exist!');
+    console.log("ERROR: task does not exist (" + opts._[0] + ")!");
     break;
   default:
     tasks[opts._[0]].fn(opts);
@@ -80,6 +89,10 @@ try {
   default:
     console.log(e);
   }
+}
+function repeatString$(str, n){
+  for (var r = ''; n > 0; (n >>= 1) && (str += str)) if (n & 1) r += str;
+  return r;
 }
 function in$(x, xs){
   var i = -1, l = xs.length >>> 0;
--- src/live.ls
+++ src/live.ls
@@ -1,8 +1,6 @@
 require! {
-  fs,
-  livescript: ls
-  optionator
-  'prelude-ls': { List, Obj, Str }
+  fs, livescript: ls, optionator
+  'prelude-ls': { List, Obj }
 }
 
 options = [{ option: \help, alias: \h, type: \Boolean, description: '' }] 
@@ -16,20 +14,19 @@ global.option = option
 global.task = task
 
 try
-  ls.run fs.readFileSync \Livefile \utf-8
+  fs.readFileSync \Livefile \utf-8 |> ls.run
   prepend = 'Usage: live [options]\n\nOptions:'
   mlgth = Obj.keys tasks |> List.maximum-by (.length) |> (.length)
-  filler = (str) -> Str.repeat (mlgth - str.length), ' '
-  line = (acc, elt) -> "#{acc}#{elt[0]}#{filler elt[0]} #{elt[1].desc}\n"
-  append =
-    Obj.obj-to-pairs tasks
-    |> List.fold line, 'Commands:\n\n'
+  filler = (str) -> ' ' * (mlgth - str.length)
+  line = (acc, elt) -> "#{acc}#{elt[0]}#{filler elt[0]}  #{elt[1].desc}\n"
+  append = Obj.obj-to-pairs tasks |> List.fold line, 'Commands:\n\n'
   op = optionator { append, options, prepend }
   # the if/else below is for testing the lib itself
   args =
-    if (new RegExp \node.exe .test process.argv[0]) then process.argv.slice 1
+    if (new RegExp \lsc .test process.argv[1]) then process.argv.splice 1 1
     else process.argv
   opts = op.parseArgv args
+  opts = optionator { append, options, prepend } |> (.parseArgv process.argv)
   switch
     | opts.help          => console.log op.generateHelp!
     | opts._.length is 0 => console.log 'ERROR: no task to run!'
